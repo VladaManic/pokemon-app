34a2d15df209262e51f0fa153dae73e357a37238
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-09-26 13:21:11 +0200

    folder & file structure

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <h1>Pokemon App</h1>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/layout/Main/index.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../../pages/Login";
+import Home from "../../pages/Home";
+import Single from "../../pages/Single";
+
+const Main = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<Single />} />
+      </Routes>
+    </>
+  );
+};
+
+export default Main;
