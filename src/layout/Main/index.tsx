54bb435acd5cc0bf76343cce212f7c9996ed49dd
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-09-29 12:23:30 +0200

    prettier corrections

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -1,19 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 
-import Login from "../../pages/Login";
-import Home from "../../pages/Home";
-import Single from "../../pages/Single";
+import Login from '../../pages/Login'
+import Home from '../../pages/Home'
+import Single from '../../pages/Single'
 
 const Main = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home/" element={<Home />} />
-        <Route path="/pokemon/:id" element={<Single />} />
-      </Routes>
-    </>
-  );
-};
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home/" element={<Home />} />
+                <Route path="/pokemon/:id" element={<Single />} />
+            </Routes>
+        </>
+    )
+}
 
-export default Main;
+export default Main
