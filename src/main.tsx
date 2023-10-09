a7ec650a8811fddadf6d99c79aad5ad7046094f1
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-10-09 11:58:17 +0200

    fetch once for all except fetch by color

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,21 @@ import { ColoredPokemonsContextProvider } from './context/ColoredPokemonsContext
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const twentyFourHoursInMs = 1000 * 60 * 60 * 24
+
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    //Setting default paramethers for fetching data only once (if already fetched, data is called from cache)
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            retry: false,
+            staleTime: twentyFourHoursInMs,
+        },
+    },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useContext, useEffect } from 'react'
 import ColoredPokemonsContext from '../../context/ColoredPokemonsContext'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, QueryClient } from '@tanstack/react-query'
 import { getPokemonList } from '../../helpers/tanStackQuery'
 import axios from 'axios'
 
@@ -26,7 +26,7 @@ const Home = () => {
         ],
         queryFn: () =>
             getPokemonList(coloredPokemonsCtx, currentPage, selectedColor),
-        cacheTime: 15 * (60 * 1000), // 15 mins of caching data in local cache (if cacheTime not set, default is 5min)
+        //cacheTime: 15 * (60 * 1000), // 15 mins of caching data in local cache (if cacheTime not set, default is 5min)
     })
 
     //Refetch if error when trying fo get paginated list of all pokemons
